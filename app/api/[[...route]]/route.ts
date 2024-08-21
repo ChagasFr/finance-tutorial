1fb1e8402be6280ae3766426dfb5bdf2f47b21f8
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-08-21 16:35:19 -0300

    Fix:  ajust routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,30 +1,12 @@
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
-
-app.get("/hello/:test", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({ error: "Unauthorzied" });
-  }
-
-  return c.json({
-    message: "Hello Next.js!",
-    userId: auth.userId,
-  });
+app.get("hello", (c) => {
+  return c.json({ hello: "world" });
 });
 
 export const GET = handle(app);
