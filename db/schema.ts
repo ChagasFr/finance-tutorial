62c5523d3ac253afc5282b29cb5839a27091e45c
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-08-24 17:53:43 -0300

    Feat: db

--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,7 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const account = pgTable("accounts", {
   id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
   name: text("name").notNull(),
   userId: text("user_is").notNull(),
 });
