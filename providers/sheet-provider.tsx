c58f7b8bd2d853d18c0dd648fbca231bdd91f38e
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-15 20:25:53 -0300

    Feat: sheet provider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -2,10 +2,18 @@
 
 import { useMountedState } from "react-use"
 
+import { useState, useEffect } from "react"
+
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 
 export const SheetProvider = () => {
-    const isMounted = useMountedState();
+    // const isMounted = useMountedState();
+
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
     if (!isMounted) return null
 
