353a8452789ea1cb6a62f6b006f2706871a0b75c
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-16 19:34:54 -0300

    fix: ajus dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,19 +1,12 @@
 "use client"
 
-import useGetAccounts from "@/features/accounts/api/use-get-accounts"
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
-  const accountsQuery = useGetAccounts();
+  const { isOpen, onClose } = useNewAccount();
 
   return (
     <div>
-      {accountsQuery.data?.map((x) => {
-        return (
-          <div>
-
-          </div>
-        )
-      })}
     </div>
   )
 }
