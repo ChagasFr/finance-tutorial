e3394caf89d27226ded34cd93cdc35a356d02b79
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-18 13:05:51 -0300

    Feat: accounts page

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 const AccountsPage = () => {
+    const newAccount = useNewAccount();
+
     return (
         <div>
-            <Card>
-                Accounts Page
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Accounts Page
+                    </CardTitle>
+                    <Button onClick={newAccount.onOpen} size="sm">
+                        <Plus className="size-4 mr-2" />
+                        Add New
+                    </Button>
+                </CardHeader>
             </Card>
         </div>
     )
