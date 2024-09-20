119c0ee49aadf8493fec9092e32c76ffbe4d6338
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-20 16:15:50 -0300

    Feat: hook use-confirm

---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const useConfirm = (
+    title: string,
+    message: string,
+): [() => JSX.Element, () => Promise<unknown>] => {
+    const [promise, setPromise] = useState<{
+        resolve: (values: boolean) =>
+            void
+    } | null>(null);
+
+    const confirm = () => new Promise((resolve, reject) => {
+        setPromise({ resolve });
+    });
+
+    const handleClose = () => {
+        setPromise(null);
+    };
+
+    const handleConfirm = () => {
+        promise?.resolve(true);
+        handleClose();
+    };
+
+    const handleCancel = () => {
+        promise?.resolve(false);
+        handleClose();
+    };
+};
