ceb28d597ef36e596a792980e65556dcb781dc59
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-20 12:15:43 -0300

    feat: api route

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -2,7 +2,7 @@
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -20,11 +20,16 @@ const AccountsPage = () => {
             <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
                 <Card className="border-none drop-shadow-sm">
                     <CardHeader >
-                        <Skeleton />
+                        <Skeleton className="h-8 w-48" />
                     </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
                 </Card>
             </div>
-        )
+        );
     }
 
     return (
