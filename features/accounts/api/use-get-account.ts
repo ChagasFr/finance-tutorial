681932754f4f3196e4c73c97245050e5d6e8aee0
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-24 13:05:41 -0300

    feat: get and edit account

--- features/accounts/api/use-get-account.ts
+++ features/accounts/api/use-get-account.ts
@@ -5,12 +5,14 @@ import { client } from "@/lib/hono";
 export const useGetAccount = (id?: string) => {
   const query = useQuery({
     enabled: !!id,
-    queryKey: ["accounts", { id }],
+    queryKey: ["account", { id }],
     queryFn: async () => {
-      const response = await client.api.accounts.$get();
+      const response = await client.api.accounts[":id"].$get({
+        param: { id },
+      });
 
       if (!response.ok) {
-        throw new Error("Failed to fetch accounts");
+        throw new Error("Failed to fetch account");
       }
 
       const { data } = await response.json();
