b2bfee66771105d4297181fd8765d10c4cfb5eee
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-10-14 22:34:30 -0300

    feat: ajust features hooks

--- features/transactions/components/new-transaction-sheet.tsx
+++ features/transactions/components/new-transaction-sheet.tsx
@@ -4,6 +4,7 @@ import { useNewTransaction } from "../hooks/use-new-transaction";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
 import { insertTransactionSchema } from "@/db/schema";
 import { useCreateTransaction } from "../api/use-create-transaction";
+import { useCreateCategory } from "@/features/categories/api/use-create-category";
 
 const formSchema = insertTransactionSchema.omit({
     id: true,
@@ -15,6 +16,8 @@ export const NewTransactionSheet = () => {
     const { isOpen, onClose } = useNewTransaction();
 
     const mutation = useCreateTransaction();
+
+    const categoryMutation = useCreateCategory();
 
     const onSubmit = (values: FormValues) => {
         mutation.mutate(values, {
--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useMountedState } from "react-use"
 
 import { useState, useEffect } from "react"
@@ -8,6 +7,8 @@ import { NewAccountSheet } from "@/features/accounts/components/new-account-shee
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
 
 export const SheetProvider = () => {
     const isMounted = useMountedState();
@@ -21,6 +22,8 @@ export const SheetProvider = () => {
 
             <NewCategorySheet />
             <EditCategorySheet />
+
+            <NewTransactionSheet />
         </>
     )
 }
