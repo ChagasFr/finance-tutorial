568ae22462b798dd5ba49c55eaa2360582412f53
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-11-04 20:02:50 -0300

    feat: amount input

--- components/amount-input.tsx
+++ components/amount-input.tsx
@@ -4,6 +4,7 @@ import { Info, MinusCircle, PlusCircle } from "lucide-react";
 import { cn } from "@/lib/utils"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 import { placeholder } from "drizzle-orm";
+import CurrencyInput from "react-currency-input-field";
 
 type Props = {
     value: string;
@@ -37,12 +38,24 @@ export const AmountInput = ({
                     <TooltipTrigger asChild>
                         <button type="button" onClick={onReverseValue} className={cn("bg-slate-400 hover:bg-slate-500 absolute top-1.5 rounded-md p-2 flex items-center justify-center transition")}>
                             {!parsedValue && <Info className="size-3 text-white" />}
-                            {!parsedValue && <PlusCircle className="size-3 text-white" />}
-                            {!parsedValue && <MinusCircle className="size-3 text-white" />}
+                            {isIncome && <PlusCircle className="size-3 text-white" />}
+                            {isExpense && <MinusCircle className="size-3 text-white" />}
                         </button>
                     </TooltipTrigger>
+                    <TooltipContent>
+                        Use [+] for income and [-] for expenses
+                    </TooltipContent>
                 </Tooltip>
             </TooltipProvider>
+            <CurrencyInput prefix="$"
+                className="pl-10 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                placeholder={placeholder}
+                value={value}
+                decimalsLimit={2}
+                decimalScale={2}
+                onValueChange={onChange}
+                disabled={disabled}
+            />
         </div>
     )
 }
