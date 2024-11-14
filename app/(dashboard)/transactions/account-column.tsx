3413de54a3b70f425befd2ff202b47ad34d1f207
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-11-14 19:36:41 -0300

    feat: account column

--- app/(dashboard)/transactions/account-column.tsx
+++ app/(dashboard)/transactions/account-column.tsx
@@ -4,15 +4,20 @@ import { cn } from "@/lib/utils";
 
 type Props = {
     id: string;
-    account: string | null;
-    accountId: string | null;
+    account: string;
+    accountId: string;
 }
 
 export const AccountColumn = ({
-    id,
     account,
     accountId
 }: Props) => {
+    const { onOpen: onOpenAccount } = useOpenAccount();
+
+    const onClick = () => {
+        onOpenAccount(accountId);
+    }
+
     return (
         <div className="flex items-center cursor-pointer hover:underline">
             {account}
