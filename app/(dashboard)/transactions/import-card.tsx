26a9c1f5f1dfda22c5527461a0aba0bb0827622e
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-12-07 23:23:04 -0300

    feat: import card dashboard

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -1,4 +1,14 @@
-export const ImportCard = () => {
+type Props = {
+    data: string[][];
+    onCancel: () => void;
+    onSubmit: (data: any) => void
+}
+
+export const ImportCard = ({
+    data,
+    onCancel,
+    onSubmit,
+}: Props) => {
     return (
         <div>
             Import Card
