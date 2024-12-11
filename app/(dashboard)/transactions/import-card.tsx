a4ddffd1c13d598e627d16e1ee8a43f562aea70d
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-12-10 22:58:42 -0300

    feat: import card dashboard

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -1,3 +1,6 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 const dateFormat = "yyyy-MM-dd HH:mm:ss";
 const outputFormat = "yyyy-MM-dd";
 
@@ -23,8 +26,24 @@ export const ImportCard = ({
     onSubmit,
 }: Props) => {
     return (
-        <div>
-            Import Card
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Transactions History
+                    </CardTitle>
+                    <div className="flex items-center gap-x-2">
+                        <Button onClick={newTransaction.onOpen} size="sm">
+                            <Plus className="size-4 mr-2" />
+                            Add New
+                        </Button>
+                        <UploadButton onUpload={onUpload} />
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    Hello
+                </CardContent>
+            </Card>
         </div>
     )
 }
