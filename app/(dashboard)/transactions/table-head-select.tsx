544c3816f85002c60ba90e854aa42b70afc5b5ac
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-12-27 23:10:19 -0300

    Feat: trigger select

--- app/(dashboard)/transactions/table-head-select.tsx
+++ app/(dashboard)/transactions/table-head-select.tsx
@@ -1,3 +1,6 @@
+import { Select } from "@/components/select";
+import { cn } from "@/lib/utils";
+
 type Props = {
     columnIndex: number;
     selectedColumns: Record<string, string | null>;
@@ -19,9 +22,22 @@ export const TableHeadSelect = ({
     selectedColumns,
     onChange
 }: Props) => {
+    const currentSelect = selectedColumns[`column_${columnIndex}`];
+
     return (
-        <div>
-            Select
-        </div>
+        <Select
+            value={currentSelection || ""}
+            onValueChange={(value) => onChange(columnIndex, value)}
+        >
+            <SelectTrigger
+                className={cn(
+                    "focus:ring-offset-0 focus:ring-transparent outilne-none"
+                )}
+            >
+
+            </SelectTrigger>
+
+        </Select>
+
     )
 }
