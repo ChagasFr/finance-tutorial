3f8fc4614f108646040d8d87f9440f2f9bf8b836
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-01-06 16:57:53 -0300

    feat: summary route

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -1,7 +1,8 @@
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { zValidator } from "@hono/zod-validator";
-import { Hono } from "hono";
 import { z } from "zod";
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { subDays, parse, differenceInDays } from "date-fns";
 
 const app = new Hono().get(
   "/",
@@ -21,6 +22,31 @@ const app = new Hono().get(
     if (!auth?.userId) {
       return c.json({ error: "Unauthorized" }, 401);
     }
+
+    const defaultTo = new Date();
+    const defaultFrom = subDays(defaultTo, 30);
+
+    const startDate = from
+      ? parse(from, "yyyy-MM-dd", new Date())
+      : defaultFrom;
+
+    const endDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+
+    const periodLength = differenceInDays(endDate, startDate) + 1;
+    const lastPeriodStart = subDays(startDate, periodLength);
+    const lastPeriodEnd = subDays(endtDate, periodLength);
+
+    const [currentPeriod] = await fetchFincancialDate(
+      auth.userId,
+      startDate,
+      endDate
+    );
+
+    const [lastPeriod] = await fetchFincancialDate(
+      auth.userId,
+      startDate,
+      endDate
+    );
   }
 );
 
