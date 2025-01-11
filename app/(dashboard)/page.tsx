d6176aa0a936a29cb6a5ff605b0a62f3fe9d1782
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-01-11 12:11:14 -0300

    Feat: data grid

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,17 +1,8 @@
-"use client"
-
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-
-
-export default function Home() {
-  const { onOpen } = useNewAccount();
+export default function DashboardPage() {
 
   return (
-    <div>
-      <Button onClick={onOpen}>
-        Add an Account
-      </Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 mt-24">
+      <dataGrid />
     </div>
   )
 }
---
+++ components/data-grid.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const DataGrid = () => {
+
+    return (
+        <div className="grid grid-cols01 lg:grid-cols-3 gap-8 pb-2 mb-8">
+            Data Grid
+        </div>
+    )
+}
