4563751dc806f1ac15d29e9c20aabaceed8d2d8b
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-01-11 21:01:49 -0300

    Feat: data card

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,15 +1,30 @@
 "use client";
-
 import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+
+import useGetSummary from "@/features/summary/api/use-get-summary";
+
+import { formatDateRange } from "@/lib/utils";
 
 export const DataGrid = () => {
+    const { data } = useGetSummary();
+
     const params = useSearchParams();
     const to = params.get("to") || undefined;
     const from = params.get("from") || undefined;
 
+    const dateRangeLabel = formatDateRange({ to, from });
+
     return (
         <div className="grid grid-cols01 lg:grid-cols-3 gap-8 pb-2 mb-8">
-            Data Grid
+            <DataCard
+                title="Remaing"
+                value={data?.remainingAmount}
+                parcentageChange={data?.remainingChange}
+                icon={FaPiggyBank}
+                variant="default"
+                dateRange={dateRangeLabel}
+            />
         </div>
     )
 }
