fb1f75549a8671117cbcf38b455f37b267c8ad40
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-01-11 22:09:01 -0300

    Feat: data card

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const boxVariant = cva(
-    "roudende-md p-3",
+    "rounded-md p-3",
     {
         variants: {
             variant: {
@@ -13,9 +13,35 @@ const boxVariant = cva(
                 danger: "bg-blue-500/20",
                 warning: "bg-blue-500/20",
             }
-        }
-    }
-)
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    },
+);
+
+const iconVariant = cva(
+    "size-6",
+    {
+        variants: {
+            variant: {
+                default: "fill-blue-500",
+                sucess: "fill-blue-500",
+                danger: "fill-blue-500",
+                warning: "fill-blue-500",
+            }
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    },
+);
+
+type BoxVariants = VariantProps<typeof boxVariant>;
+type IconVariants = VariantProps<typeof iconVariant>;
+
+interface DataCardProps extends BoxVariants, IconVariants
+
 export const DataCard = () => {
     return (
         <div>
