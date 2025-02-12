d9632c58615739af597cd3ccff7f6c87bbf6eca0
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-02-12 00:12:09 -0300

    feat: chart loading components

--- components/chart.tsx
+++ components/chart.tsx
@@ -86,5 +86,21 @@ export const Chart = ({ data = [] }: Props) => {
                 )}
             </CardContent>
         </Card>
+    );
+};
+
+export const ChartLoading = () => {
+    return (
+        <Card className="border-none drop-shadow-sm">
+            <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+                <Skeleton className="h-8 w-48" />
+                <Skeleton className="h-8 lg:w-[120px] 2-full" />
+            </CardHeader>
+            <CardContent>
+                <div className="h-[350px] w-full flex items-center justify-center">
+                    <Loader2 className="h-6 2-6 text-slate-300 animate-spin" />
+                </div>
+            </CardContent>
+        </Card>
     )
 }
